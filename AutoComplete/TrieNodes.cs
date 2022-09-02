8dc83e13c09d26189e6657d236d9be3274074502
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-09-02 09:53:19 +0200

    Implemented the Class for storing Nodes

---
+++ AutoComplete/TrieNodes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoComplete
+{
+    public class TrieNodes
+    {
+        public string Prefix { get; set; }
+        public Dictionary<char, TrieNodes> Children { get; set; }
+
+        public bool IsWord;
+
+        public TrieNodes(String prefix)
+        {
+            this.Prefix = prefix;
+            this.Children = new Dictionary<char, TrieNodes>();
+        }
+    }
+}
